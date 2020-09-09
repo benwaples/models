a723215fcf13b091456aa69d6fea4d4488c165a8
bwapes <benwaples@gmail.com>
2020-09-08 17:57:44 -0700

    fruit POST route made and tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,24 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Fruit = require('../lib/models/fruits');
 
-describe('models routes', () => {
+describe('demo routes', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
   });
+
+  it('creates a new Fruit via POST ', async() => {
+    const response = await request(app)
+      .post('/api/fruits')
+      .send({ name: 'apple', color: 'red', size: 'medium' });
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      name: 'apple',
+      color: 'red',
+      size: 'medium'
+    });
+  });
+  
 });
