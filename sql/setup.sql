2778a0e21ded36669c28343309832138c6581255
bwapes <benwaples@gmail.com>
2020-09-08 17:46:09 -0700

    beers insert

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS vegetables;
 DROP TABLE IF EXISTS fruits;
 DROP TABLE IF EXISTS nuts;
 DROP TABLE IF EXISTS essential_oils;
+DROP TABLE IF EXISTS beers;
 
 CREATE TABLE vegetables (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -30,3 +31,10 @@ CREATE TABLE essential_oils (
   color TEXT CHECK ( color = ANY('{red,green,blue,yellow,red,purple}')),
   recommend BOOLEAN
 );
+
+CREATE TABLE beers (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL,
+  type TEXT CHECK ( type = ANY('{ale,ipa,pilsner,kolsh,sour}')),
+  turnt_scale INT 
+)
